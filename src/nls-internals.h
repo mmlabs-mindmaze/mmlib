c26244e5dc92dff43f88b23e1b2a7757f4ff5fe7
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2013-07-03 15:47:39 +0200

    Add support for "--disable-nls"
    Passing "--disable-nls" at configure time now disable the translation
    altogether.

--- src/nls-internals.h
+++ src/nls-internals.h
@@ -7,10 +7,24 @@
 #ifndef NLS_INTERNALS_H
 #define NLS_INTERNALS_H
 
+#if ENABLE_NLS
+
+
 #include <libintl.h>
 
 #define N_(msg)	msg
 #define _(msg)	dgettext(PACKAGE_NAME, msg)
 #define _domaindir(podir)	bindtextdomain(PACKAGE_NAME, podir)
+
+
+#else // ENABLE_NLS
+
+
+#define N_(msg) msg
+#define _(msg)	msg
+#define _domaindir(podir) (void)(0)
+
+
+#endif // ENABLE_NLS
 
 #endif /* NLS_INTERNALS_H */
