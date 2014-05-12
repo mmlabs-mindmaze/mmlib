d66bbe0a8f2858d7544c09f5b3e3f3ea38b085a2
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2014-05-12 14:28:46 +0200

    Add struct mm_imgdesc and related functions
    Creates a new type called struct mm_imgdesc which corresponds to an image
    description, ie pointer to data buffer is not provided. This description is
    different to mmimage in respect to the pixel type description (use a pixel
    format) and to the support of a stride parameter that allows to manipulate
    non-continous image buffer.
    
    With this image description comes 3 helper functions:
     - size_t mmimg_pixel_size(unsigned int pixel_format)
     - int mmimg_set_stride(struct mm_imgdesc* img, size_t alignment)
     - void* mmimg_alloc_buffer(const struct mm_imgdesc* img)

--- src/mmtype.h
+++ src/mmtype.h
@@ -51,9 +51,83 @@ typedef struct mmquat {
 	int confidence;
 } mmquat;
 
+/**
+ * DOC: Pixel formats
+ *
+ * The formats code are formed by using 2 parts which indicated how to
+ * interprete the pixel data. The first part how the components are stored:
+ * in integer of 8, 16 or 32 bits, float or double. The second part
+ * indicates in which order, which components must be read.
+ *
+ * Following this rules, the following table describes the components and
+ * memory layout of some pixel format
+ *
+ *              format                 |  Comp.  |      memory layout
+ * ------------------------------------------------------------------------
+ * MM_PIXFMT_COMP_BGRA|MM_PIXFMT_UINT8 | R,G,B,A | B0 G0 R0 A0 B1 G1 R1 A1
+ * MM_PIXFMT_COMP_RGBA|MM_PIXFMT_UINT8 | R,G,B,A | R0 G0 B0 A0 R1 G1 B1 A1
+ * MM_PIXFMT_COMP_MONO|MM_PIXFMT_UINT16|   mono  | M0 M0 M1 M1 M2 M2 M3 M4
+ * MM_PIXFMT_COMP_HSV|MM_PIXFMT_UINT16 |  H,S,V  | H0 H0 S0 S0 V0 V0 H1 H1
+ *
+ * MM_PIXFMT_RGBA (MM_PIXFMT_COMP_BGRA | MM_PIXFMT_UINT8) is then the same
+ * as in OpenCV, but beware, it is the same as ARGB in Qt (on little endian
+ * platform).
+ */
+#define MM_PIXFMT_DATATYPE_MASK	0x000000FF
+#define MM_PIXFMT_UINT8         0x00000001
+#define MM_PIXFMT_UINT16        0x00000002
+#define MM_PIXFMT_UINT32        0x00000003
+#define MM_PIXFMT_UINT64        0x00000004
+#define MM_PIXFMT_FLOAT         0x00000005
+#define MM_PIXFMT_DOUBLE        0x00000006
+
+#define MM_PIXFMT_COMP_MASK     0x0000FF00
+#define MM_PIXFMT_COMP_MONO     0x00000100
+#define MM_PIXFMT_COMP_RGB      0x00000200
+#define MM_PIXFMT_COMP_BGR      0x00000300
+#define MM_PIXFMT_COMP_RGBA     0x00000400
+#define MM_PIXFMT_COMP_BGRA     0x00000500
+#define MM_PIXFMT_COMP_HSV      0x00000600
+#define MM_PIXFMT_COMP_HSVA     0x00000700
+#define MM_PIXFMT_COMP_HLS      0x00000800
+#define MM_PIXFMT_COMP_HLSA     0x00000900
+
+/* Usual pixel formats */
+#define MM_PIXFMT_MONO8         (MM_PIXFMT_COMP_MONO | MM_PIXFMT_UINT8)
+#define MM_PIXFMT_MONO16        (MM_PIXFMT_COMP_MONO | MM_PIXFMT_UINT16)
+#define MM_PIXFMT_BGRA          (MM_PIXFMT_COMP_BGRA | MM_PIXFMT_UINT8)
+#define MM_PIXFMT_RGBA          (MM_PIXFMT_COMP_RGBA | MM_PIXFMT_UINT8)
+#define MM_PIXFMT_BGR           (MM_PIXFMT_COMP_BGR  | MM_PIXFMT_UINT8)
+#define MM_PIXFMT_RGB           (MM_PIXFMT_COMP_RGB  | MM_PIXFMT_UINT8)
+
+
+/**
+ * struct mm_imgdesc - image buffer access description
+ * @height:     Number of lines in the images
+ * @width:      Number of columns in the image
+ * @stride:     Number of bytes between two consecutive lines.
+ * @pixformat:  Format of the pixels
+ *
+ * The field @stride allows to determine the starting address of each line:
+ * if addr is the memory address of the first pixel of line, addr+stride is
+ * the memory address of the first pixel of the next line. To have a
+ * properly formatted description, this field must be as long as or longer
+ * than the width multiplied by pixel size.
+ */
+struct mm_imgdesc {
+	int height;
+	int width;
+	unsigned int stride;
+	unsigned int pixformat;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+size_t mmimg_pixel_size(unsigned int pixel_format);
+int mmimg_set_stride(struct mm_imgdesc* img, size_t alignment);
+void* mmimg_alloc_buffer(const struct mm_imgdesc* img);
 
 size_t mmimage_buffer_size(const mmimage* img);
 
