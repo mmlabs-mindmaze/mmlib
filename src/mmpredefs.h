db68bf5c6d574b84a1af0c0196c0ddd8f2299dc0
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2017-08-24 15:33:36 +0200

    Declare MM_CONSTRUCTOR() and MM_DESTRUCTOR()
    Provides fonction declaration macros MM_CONSTRUCTOR() and
    MM_DESTRUCTOR() in mmpredefs.h exported header so that user of mmlib can
    use them at their advantage. MM_CONSTRUCTOR macro causes the defined
    function to be called automatically before execution enters main() or at
    dynamic load of library if in shared library. Similarly, MM_DESTRUCTOR()
    attribute causes the function to be called automatically after main()
    completes, exit() is called or when the shared library is unloaded
    dynamically.
    
    Refs MMLAB-1

--- src/mmpredefs.h
+++ src/mmpredefs.h
@@ -26,4 +26,40 @@
 # endif
 #endif
 
+
+/*
+ Macros MM_CONSTRUCTOR() / MM_DESTRUCTOR() to declare constructors and
+ destructors. Those function are typically called before and after main in
+ case of normal executable, or at time of load/unload in case of dynamic
+ module (ie in the case of dlopen()/LoadLibrary())
+*/
+#if defined(__GNUC__)
+
+#  define MM_CONSTRUCTOR(name) static void __attribute__((constructor)) name ## _ctor(void)
+#  define MM_DESTRUCTOR(name) static void __attribute__((destructor)) name ## _dtor(void)
+
+#elif defined(_MSC_VER)
+
+#include <stdlib.h>
+
+#  define MM_CONSTRUCTOR(name) \
+  static void name ## _ctor(void); \
+  static int name ## _ctor_wrapper(void) { name ## _ctor(); return 0; } \
+  __pragma(section(".CRT$XCU",read)) \
+  __declspec(allocate(".CRT$XCU")) static int (* array_ctor_ ## name)(void) = name ## _ctor_wrapper; \
+  static void name ## _ctor(void)
+
+#  define MM_DESTRUCTOR(name) \
+  static void name ## _dtor(void); \
+  static int name ## _reg_dtor_wrapper(void) { atexit(name ## _dtor); return 0; } \
+  __pragma(section(".CRT$XCU",read)) \
+  __declspec(allocate(".CRT$XCU")) static int (* array_dtor_ ## name)(void) = name ## _reg_dtor_wrapper; \
+  static void name ## _dtor(void)
+
+#else
+
+#  warning MM_CONSTRUCTOR() cannot be defined
+
+#endif
+
 #endif
