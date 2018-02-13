957706bbc141a3e5d23f6e2db2e6b762e23730e6
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2018-02-13 12:19:20 +0100

    Use tests-child-proc directly not the lt-wrapped one
    When spawning test child process in testapi, run directly the executable
    tests-child-proc, not the libtool wrapper. This avoid the wrapper to
    mess the unicode arguments on win32 and is actually useless since the
    execution environment has already been setup for testapi process.
    
    Refs MMLAB-26

--- tests/tests-child-proc.h
+++ tests/tests-child-proc.h
@@ -4,7 +4,7 @@
 #ifndef TESTS_CHILD_PROC_H
 #define TESTS_CHILD_PROC_H
 
-#define TESTS_CHILD_BIN	BUILDDIR"/tests-child-proc"EXEEXT
+#define TESTS_CHILD_BIN	BUILDDIR"/"LT_OBJDIR"tests-child-proc"EXEEXT
 #define WR_PIPE_FD	3
 #define RD_PIPE_FD	4
 
