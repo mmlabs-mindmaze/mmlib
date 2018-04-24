40c3fdaf1349c4c4a58d9a9bd32e9b802381c2d1
Gabriel Ganne <gabriel.ganne@mindmaze.ch>
2018-04-24 10:01:07 +0200

    dlfcn api doc: move doc from header to implementation
    Change-Id: Id8c42bfb68d1d6631862882acfbc345096bef97c

--- src/dlfcn.c
+++ src/dlfcn.c
@@ -139,6 +139,12 @@ void* arch_dlsym(mmdynlib_t* handle, const char* symbol)
  *                                                                        *
  **************************************************************************/
 
+
+/**
+ * mm_dl_fileext() - get usual shared library extension
+ *
+ * Return: the usual shared library extension of the platform.
+ */
 API_EXPORTED
 const char* mm_dl_fileext(void)
 {
@@ -148,6 +154,43 @@ const char* mm_dl_fileext(void)
 }
 
 
+/**
+ * mm_dlopen() - Load library dynamically
+ * @path:       path of the library to load
+ * @flags:      flags controlling how the library is loaded
+ *
+ * This function makes the symbols (function identifiers and data object
+ * identifiers) in the shared library specified by @path available to the
+ * calling process. A successful mm_dlopen() returns an handle which the
+ * caller may use on subsequent calls to mm_dlsym() and mm_dlclose().
+ *
+ * The behavior of the function can be controlled by @flags which must be a
+ * OR-combination of the following flags:
+ *
+ * If path is NULL, this will return a handle to the main program.
+ *
+ * MMLD_LAZY
+ *   Relocations shall be performed at an implementation-defined time,
+ *   ranging from the time of the imm_dlopen() call until the first reference to
+ *   a given symbol occurs. Currently, this has no effect on Windows
+ *   platform.
+ *
+ * MMLD_NOW
+ *   All necessary relocations shall be performed when shared library is
+ *   first loaded. This may waste some processing if relocations are
+ *   performed for symbols that are never referenced. This behavior may be
+ *   useful for applications that need to know that all symbols referenced
+ *   during execution will be available before mm_dlopen() returns.
+ *   Currently this is the only possible behavior for Windows platform.
+ *
+ * MMLD_APPEND_EXT
+ *   If set, mm_dlopen() append automatically the usual file extension
+ *   of a shared library (OS dependent) to @path and load this file instead.
+ *   This flag allows to write code that is fully platform independent.
+ *
+ * Return: In case of success, mm_dlopen() return a non-NULL handle.
+ * Otherwise NULL is returned and error state is set accordingly.
+ */
 API_EXPORTED
 mmdynlib_t* mm_dlopen(const char* path, int flags)
 {
@@ -180,6 +223,19 @@ mmdynlib_t* mm_dlopen(const char* path, int flags)
 }
 
 
+/**
+ * mm_dlclose() - Close an handle of an dynamic library
+ * @handle:     handle of library to close
+ *
+ * this informs the system that the library specified by handle is no longer
+ * needed by the application.  Once the handle has been closed, an
+ * application should assume that any symbols (function identifiers and data
+ * object identifiers) made visible using @handle, are no longer available
+ * to the process.
+ *
+ * Return: 0 in case of success, -1 otherwise with error state set
+ * accordingly
+ */
 API_EXPORTED
 void mm_dlclose(mmdynlib_t* handle)
 {
@@ -190,6 +246,18 @@ void mm_dlclose(mmdynlib_t* handle)
 }
 
 
+/**
+ * mm_dlsym() - get the address of a symbol from library handle
+ * @handle:     handle of library
+ * @symbol:     function or a data object identifier
+ *
+ * This obtain the address of a symbol (a function identifier or a data
+ * object identifier) defined in the symbol table identified by the handle
+ * argument.
+ *
+ * Return: pointer to the symbol if found, NULL otherwise with error state
+ * set acccordingly
+ */
 API_EXPORTED
 void* mm_dlsym(mmdynlib_t* handle, const char* symbol)
 {
