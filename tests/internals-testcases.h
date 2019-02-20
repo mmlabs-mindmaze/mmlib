113616339503de3d1f5e3479f1bb0be64f7d6c62
Nicolas Bourdaud <nicolas.bourdaud@mindmaze.ch>
2019-02-20 10:42:39 +0100

    tests: split testinternals implementation
    Move tests in their own compilation unit. This is needed for added more
    internal test from different C files of mmlib.
    
    Change-Id: I4bdf6d5c698301c5a312e2ef2b7a306cafb1f90e

---
+++ tests/internals-testcases.h
@@ -0,0 +1,12 @@
+/*
+   @mindmaze_header@
+*/
+#ifndef INTERNALS_TESTCASES_H
+#define INTERNALS_TESTCASES_H
+
+#include <check.h>
+
+TCase* create_case_log_internals(void);
+
+#endif
+
---
+++ tests/log-internals.c
@@ -0,0 +1,70 @@
+/*
+   @mindmaze_header@
+*/
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "../src/log.c"
+
+#include <check.h>
+#include <stdarg.h>
+
+#include "internals-testcases.h"
+
+
+// Not used in the test... Define to a dummy function just for the sake of a
+// successful compilation
+API_EXPORTED
+ssize_t mm_write(int fd, const void* buf, size_t nbyte)
+{
+	(void)fd;
+	(void)buf;
+	(void)nbyte;
+
+	return -1;
+}
+
+
+static
+size_t format_string(char* buff, size_t buflen, const char* msg, ...)
+{
+	size_t r;
+	va_list args;
+
+	va_start(args, msg);
+	r = format_log_str(buff, buflen, MMLOG_DEBUG, "here", msg, args);
+	va_end(args);
+
+	return r;
+}
+
+
+START_TEST(log_overflow)
+{
+	size_t len;
+	char buff[MMLOG_LINE_MAXLEN + 32];
+	char arg[MMLOG_LINE_MAXLEN + 32];
+
+	// Fill a message that must overflow the log string
+	memset(arg, 'a', sizeof(arg)-1);
+	arg[sizeof(arg)-1] = '\0';
+
+	// Format log string
+	len = format_string(buff, MMLOG_LINE_MAXLEN, "hello %s", arg);
+
+	ck_assert(len <= MMLOG_LINE_MAXLEN);
+	ck_assert(buff[len-1] == '\n');
+}
+END_TEST
+
+
+LOCAL_SYMBOL
+TCase* create_case_log_internals(void)
+{
+	TCase *tc = tcase_create("log internals");
+	tcase_add_test(tc, log_overflow);
+
+	return tc;
+}
+
