3ff7d343e434ce84abd2962db3c5264af348a755
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2019-10-28 16:49:28 +0100

    doc - do not generate mmlib.1 man page
    We never wanted it. It probably is a remain of some test phase with linuxdoc
    
    Change-Id: I300e40eac0bd00a2ffc8309217d3278470bad72d

--- doc/conf.py
+++ doc/conf.py
@@ -312,8 +312,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'mmlib', u'mmlib Documentation',
-     [author], 1)
 ]
 
 # If true, show URL addresses after external links.
