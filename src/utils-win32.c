8400fe3573648446055f482a0f1ee5ce8aff853c
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2021-09-01 13:09:38 +0200

    win32: ERROR_NOT_SUPPORTED -> ENOTSUP
    Change-Id: I384988367cda9059988551ebd284f38f4078e355

--- src/utils-win32.c
+++ src/utils-win32.c
@@ -596,6 +596,7 @@ int get_errcode_from_w32err(DWORD w32err)
 	case ERROR_NOT_SAME_DEVICE:     return EXDEV;
 	case WSASYSNOTREADY:
 	case ERROR_DIRECTORY_NOT_SUPPORTED: return EISDIR;
+	case ERROR_NOT_SUPPORTED:       return ENOTSUP;
 	case WSAENETDOWN:               return ENETDOWN;
 	case WSAENETUNREACH:            return ENETUNREACH;
 	case WSAVERNOTSUPPORTED:        return ENOSYS;
